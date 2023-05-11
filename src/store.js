4b143c36a4176f395fe46bf69cad50196b53fe9d
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-05-11 12:01:24 +0300

    3 task completed

--- src/store.js
+++ src/store.js
@@ -73,6 +73,9 @@ class Store {
       list: this.state.list.map(item => {
         if (item.code === code) {
           item.selected = !item.selected;
+          if (item.selected) {
+            item.selectedCount ? item.selectedCount++ : item.selectedCount = 1;
+          }
         } else {
           item.selected = false;
         }
