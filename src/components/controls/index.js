bbb8857345b0736d409d3cd8079b9540541a073c
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-05-18 23:24:11 +0300

    add props to controls

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import { plural } from '../../utils';
-import './style.css';
+import PropTypes from "prop-types";
+import { plural } from "../../utils";
+import "./style.css";
 
-function Controls({onOpenModal, cart}) {
+function Controls({ onOpenModal, cart }) {
   const sum = cart.reduce((sum, item) => sum + item.price * item.count, 0);
   const cartValue = cart.length ? (
     <b>
       {`${cart.length} ${plural(cart.length, {
-        one: 'товар',
-        few: 'товара',
-        many: 'товаров',
+        one: "товар",
+        few: "товара",
+        many: "товаров",
       })} / ${sum} `}
       &#8381;
     </b>
@@ -18,21 +18,27 @@ function Controls({onOpenModal, cart}) {
     <b>пусто</b>
   );
   return (
-    <div className='Controls'>
+    <div className="Controls">
       <div className="Controls-cart">В корзине:&nbsp;&nbsp;{cartValue}</div>
       <div className="Controls-actions">
         <button onClick={onOpenModal}> Перейти </button>
       </div>
     </div>
-  )
+  );
 }
 
 Controls.propTypes = {
-  onOpenModal: PropTypes.func
+  cart: PropTypes.arrayOf(
+    PropTypes.shape({
+      price: PropTypes.number,
+      count: PropTypes.number,
+    })
+  ).isRequired,
+  onOpenModal: PropTypes.func,
 };
 
 Controls.defaultProps = {
-  onOpenModal: () => {}
-}
+  onOpenModal: () => {},
+};
 
 export default React.memo(Controls);
