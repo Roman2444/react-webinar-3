35f1f84c9cc67a28866eac3cf6751162a27709f7
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-05-19 13:29:09 +0300

    added propTypes to item

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -9,40 +9,36 @@ function Item(props){
     }
   };
 
-  console.log('props', props);
-
+  console.log("props22", props);
   return (
-    <div className={'Item'}
-         onClick={callbacks.onClick}>
-      <div className='Item-code'>{props.item.code}</div>
-      <div className='Item-title'>
-        {props.item.title} 
-      </div>
-      <div className='Item-price'>
-        {props.item.price} &#8381;
-      </div>
-      {props.item.count && <div className='Item-count'>{props.item.count} шт</div>} 
-      <div className='Item-actions'>
-        <button onClick={callbacks.onHandleClick}>
-          {props.buttonName}
-        </button>
+    <div className={"Item"} onClick={callbacks.onClick}>
+      <div className="Item-code">{props.item.code}</div>
+      <div className="Item-title">{props.item.title}</div>
+      <div className="Item-price">{props.item.price} &#8381;</div>
+      {props.item.count && (
+        <div className="Item-count">{props.item.count} шт</div>
+      )}
+      <div className="Item-actions">
+        <button onClick={callbacks.onHandleClick}>{props.buttonName}</button>
       </div>
     </div>
   );
 }
 
 Item.propTypes = {
+  buttonName: PropTypes.string.isRequired,
+  count: PropTypes.number,
   item: PropTypes.shape({
     code: PropTypes.number,
     title: PropTypes.string,
-    selected: PropTypes.bool,
+    price: PropTypes.number,
     count: PropTypes.number,
   }).isRequired,
-  onAddProductToCart: PropTypes.func,
+  onHandleClick: PropTypes.func,
 };
 
 Item.defaultProps = {
-  onAddProductToCart: () => {},
-}
+  onHandleClick: () => {},
+};
 
 export default React.memo(Item);
