3c06b9f728898cdd532c47568cc198f1965ab841
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-06-05 11:45:48 +0300

    corrected modifyCategoryItems added hashTable

--- src/utils.js
+++ src/utils.js
@@ -36,17 +36,21 @@ export function numberFormat(value, locale = 'ru-RU', options = {}) {
 
 // добавляет "-" в зависимости от количества родителей
 export function modifyCategoryItems(items) {
+  const hashTable = new Map();
   const newItems = [];
+
+  // Создаем хэш-таблицу, где ключ - это _id элемента, а значение - сам элемент
+  items.forEach((item) => hashTable.set(item._id, item));
 
   for (let i = 0; i < items.length; i++) {
     const currentItem = items[i];
-    
+
     if (currentItem.parent && currentItem.parent._id) {
       let parentCount = 1;
       let currentParentID = currentItem.parent._id;
 
-      while (currentParentID) {
-        const parentItem = items.find(item => item._id === currentParentID);
+      while (hashTable.has(currentParentID)) {
+        const parentItem = hashTable.get(currentParentID);
 
         if (parentItem && parentItem.parent && parentItem.parent._id) {
           parentCount++;
@@ -60,7 +64,9 @@ export function modifyCategoryItems(items) {
       currentItem.title = "-".repeat(parentCount) + " " + currentItem.title;
 
       // Находим индекс родительского элемента
-      const parentIndex = newItems.findIndex(element => element._id === currentItem.parent._id);
+      const parentIndex = newItems.findIndex(
+        (element) => element._id === currentItem.parent._id
+      );
 
       // Добавляем текущий элемент после его родителя
       newItems.splice(parentIndex + 1, 0, currentItem);
